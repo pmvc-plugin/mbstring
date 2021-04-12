7f29a4c997fa1f2a6637e5e58ebe67b12870741f
Hill <hill@kimo.com>
2021-04-12 18:49:34 +0000

    Add detect mb_string

--- mbstring.php
+++ mbstring.php
@@ -4,17 +4,24 @@ namespace PMVC\PlugIn\mbstring;
 
 use PMVC\PlugIn;
 
-${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\mbstring';
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__ . '\mbstring';
 
 class mbstring extends PlugIn
 {
+    public function init()
+    {
+        if (!function_exists('mb_convert_encoding')) {
+            die('mb_string not installed!!');
+        }
+    }
+
     public function __call($method, $args)
     {
-      $mbFunc = 'mb_'.\PMVC\camelCase($method, '_');
-      if (function_exists($mbFunc)) {
-        return call_user_func_array($mbFunc, $args);
-      } else {
-        return parent::__call($method, $args);
-      }
+        $mbFunc = 'mb_' . \PMVC\camelCase($method, '_');
+        if (function_exists($mbFunc)) {
+            return call_user_func_array($mbFunc, $args);
+        } else {
+            return parent::__call($method, $args);
+        }
     }
 }
